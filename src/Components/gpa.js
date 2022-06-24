a280f6b4b455915f6133a99fd42c93be5bbda67b
Prabhash Varma <varmaprabhash@gmail.com>
2022-06-24 16:30:12 +0530

    Update

--- src/Components/gpa.js
+++ src/Components/gpa.js
@@ -1,13 +1,15 @@
 import React from "react";
-import './gpa.css'
-export default function GPA({courseName,credits,grade}){
-    return (
-        <div className="gpa">
-           Course Name:{courseName}
-           <br></br> 
-           Credits:{credits}
-           <br></br>
-           Grade:{grade}
-        </div>
-   )
+import "./gpa.css";
+export default function GPA({ courseName, credits, grade }) {
+  return (
+    <div className="gpa">
+      <p className="gpa-title">Course Name:</p>
+      <p className="gpa-result">{courseName}</p>
+      <p className="gpa-title">Credits:</p>
+      <p className="gpa-result">{credits}</p>
+
+      <p className="gpa-title">Grade:</p>
+      <p className="gpa-result"> {grade}</p>
+    </div>
+  );
 }
